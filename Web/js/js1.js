821752151f8ac07ca72cef5747b13ade58f96483
Bryan <jbryan.699@gmail.com>
2018-12-19 22:37:35 -0600

    Se agrego los componentes para el diseño de la página web

---
+++ Web/js/js1.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#buttonTabla').click(function () {
+        $.ajax({
+            type = "POST",
+            url = "Service/Service1.svc/Productos",
+            cache = true,
+            async = false,
+            data ="{}",
+            contentType = "application/json; charset=utf-8",
+            dataType = "json",
+            success: function (resultado) {
+                var items = resultado.d;
+                $.each(items, function (index, item) {
+                    $("$tabla").append("<tr><td class=''>" + item.tkt_plu_id + "</td><td class=''>" + item.tkt_plu_long_desc + "</td>");
+                });
+            },
+            error: function (result) {
+                alert("Jadados");
+            }
+        });
+    })
+});
